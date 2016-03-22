7b9c7486e4f4435ca645aa8a49aeb130b6e80951
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2016-03-22 15:45:34 -0500

    Add simple queue implementation

--- stackQueue.js
+++ stackQueue.js
@@ -22,7 +22,41 @@ class Stack {
   }
 }
 
+
+class Queue {
+  constructor() {
+    this.in = null;
+    this.out = null;
+  }
+
+  enqueue(value) {
+    let newNode = {
+      value: value,
+      next: null,
+    };
+
+    if (this.in) {
+      this.in.next = newNode;
+    } else {
+      this.out = newNode;
+    }
+
+    this.in = newNode;
+  }
+
+  dequeue() {
+    if (!this.out) {
+      return undefined;
+    }
+    let value = this.out.value;
+    this.out = this.out.next;
+    return value;
+  }
+}
+
+
 // TEST
+
 let stack = new Stack();
 stack.push(1);
 stack.push(2);
@@ -31,3 +65,12 @@ console.log(stack.pop());
 console.log(stack.pop());
 console.log(stack.pop());
 console.log(stack.pop());
+
+let queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
