d1fba1e1fde096504709bc812e915cde5d946ade
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2016-03-22 16:57:17 -0500

    Start binary search tree

--- treeGraph.js
+++ treeGraph.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const EMPTY_TREE = {
-  height: 0,
+  height: () => 0,
+  _find: () => false,
 };
 
 class Tree {
@@ -31,3 +32,69 @@ class Tree {
 
   }
 }
+
+
+class BinarySearchTree extends Tree {
+  constructor(value) {
+    super(value);
+  }
+
+  _find(value) {
+    if (value === this.value) {
+      return this;
+
+    } else if (value < this.value) {
+      return this.left._find(value) || this;
+
+    } else if (value > this.value) {
+      return this.right._find(value) || this;
+    }
+  }
+
+  includes(value) {
+    return this._find(value).value === value;
+  }
+}
+
+
+// TEST
+
+let tree = new Tree(2);
+    tree.left = new Tree(1);
+    tree.right = new Tree(3);
+console.log(tree.height());
+console.log(tree.isBalanced());
+
+console.log();
+
+let searchTree = new BinarySearchTree(2);
+    searchTree.left = new BinarySearchTree(1);
+    searchTree.right = new BinarySearchTree(3);
+console.log(searchTree.includes(1));
+console.log(searchTree.includes(3));
+console.log(searchTree.includes(0));
+console.log(searchTree.includes(5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
