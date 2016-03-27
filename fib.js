2c7d83c0935e998f7f5ea921e35789b17d7f71ba
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2016-03-27 14:57:10 -0500

    Alter so that fib(0) = 0

--- fib.js
+++ fib.js
@@ -1,24 +1,24 @@
 'use strict';
 
 function fibNaive(n) {
-  return n < 2 ? 1 : fibNaive(n - 1) + fibNaive(n - 2);
+  return n < 2 ? n : fibNaive(n - 1) + fibNaive(n - 2);
 }
 
 let memo = {
-  0: 1,
+  0: 0,
   1: 1,
 };
 
 function fibMemoized(n) {
-  return memo[n] ? memo[n] : memo[n] = memo[n - 1] + memo[n - 2];
+  return n in memo ? memo[n] : memo[n] = memo[n - 1] + memo[n - 2];
 }
 
 function fibIterative(n) {
   if (n < 2) {
-    return 1;
+    return n;
   }
   let back1 = 1;
-  let back2 = 1;
+  let back2 = 0;
   for (let i = 2; i < n; ++i) {
     back1 = back1 + back2;
     back2 = back1 - back2;
